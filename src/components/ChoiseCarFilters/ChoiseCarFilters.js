af9cb47f54b5ce22c09534e90492451149fac838
Ivan <vanyok.korobka@ukr.net>
2022-06-27 00:28:40 +0300

    moved the ChoiseCarFilters component to a functional component

--- src/components/ChoiseCarFilters/ChoiseCarFilters.js
+++ src/components/ChoiseCarFilters/ChoiseCarFilters.js
@@ -1,45 +1,42 @@
-import { Component } from 'react';
 import PropTypes from 'prop-types';
 import ChoiseCarFiltersView from './ChoiseCarFiltersView';
 
-class ChoiseCarFilters extends Component {
-  render() {
-    const { onChangeFilter, filter } = this.props;
+function ChoiseCarFilters(props) {
+  const { onChangeFilter, filter } = props;
 
-    const buttonsData = [
-      { name: 'all', label: 'Все машины' },
-      { name: 'sedan', label: 'Седаны' },
-      { name: 'pickup', label: 'Пикапы' },
-      { name: 'universal', label: 'Универсалы' },
-      { name: 'alphabet', label: 'Сортировка машин за алфавитом' },
-    ];
+  const buttonsData = [
+    { name: 'all', label: 'Все машины' },
+    { name: 'sedan', label: 'Седаны' },
+    { name: 'pickup', label: 'Пикапы' },
+    { name: 'universal', label: 'Универсалы' },
+    { name: 'alphabet', label: 'Сортировка машин за алфавитом' },
+  ];
 
-    const buttons = buttonsData.map(({ name, label }) => {
-      const clazz = filter === name ? 'choiseCar_btn choiseCar_btn_active' : 'choiseCar_btn';
-      return (
-        <button
-          className={clazz}
-          type="button"
-          onClick={() => onChangeFilter(name)}
-          key={name}
-        >
-          {label}
-        </button>
-      );
-    });
-
+  const buttons = buttonsData.map(({ name, label }) => {
+    const clazz = filter === name ? 'choiseCar_btn choiseCar_btn_active' : 'choiseCar_btn';
     return (
-      <ChoiseCarFiltersView buttons={buttons} />
+      <button
+        className={clazz}
+        type="button"
+        onClick={() => onChangeFilter(name)}
+        key={name}
+      >
+        {label}
+      </button>
     );
-  }
+  });
+
+  return (
+    <ChoiseCarFiltersView buttons={buttons} />
+  );
 }
 
-ChoiseCarFilters.propTypes = { 
+ChoiseCarFilters.propTypes = {
   filter: PropTypes.string,
   onChangeFilter: PropTypes.func,
 };
 
-ChoiseCarFilters.defaultProps = { 
+ChoiseCarFilters.defaultProps = {
   filter: 'all',
   onChangeFilter: () => null,
 };
