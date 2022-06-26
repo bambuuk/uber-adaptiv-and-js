6807092e7d1423a93e6c90c07806ff1f0ba4cc29
Ivan <vanyok.korobka@ukr.net>
2022-06-27 00:25:22 +0300

    moved the Choise component to a functional component

--- src/components/Choise/Choise.js
+++ src/components/Choise/Choise.js
@@ -1,31 +1,28 @@
-import { Component } from 'react';
 import machine from '../../img/machine.png';
 
 import './choise.sass';
 
-class Choise extends Component {
-  render() {
-    return (
-      <section className="choise">
-        <div className="container">
-          <div className="choise_content">
-            <div className="choise_content_label">Возможности</div>
-            <h2 className="choise_content_title">Выбор - отличная штука!</h2>
-            <div className="choise_content_subtitle">
-              Закажите автомобиль, который подходит вам по стилю и бюджету
-            </div>
-            <img src={machine} alt="machine" className="choise_content_car" />
-            <div className="choise_content_text">
-              Машины на каждый день.
-              <br />
-              Лучше, быстрее и дешевле, чем такси
-            </div>
+function Choise() {
+  return (
+    <section className="choise">
+      <div className="container">
+        <div className="choise_content">
+          <div className="choise_content_label">Возможности</div>
+          <h2 className="choise_content_title">Выбор - отличная штука!</h2>
+          <div className="choise_content_subtitle">
+            Закажите автомобиль, который подходит вам по стилю и бюджету
+          </div>
+          <img src={machine} alt="machine" className="choise_content_car" />
+          <div className="choise_content_text">
+            Машины на каждый день.
+            <br />
+            Лучше, быстрее и дешевле, чем такси
           </div>
         </div>
-        <div className="substrate" />
-      </section>
-    );
-  }
+      </div>
+      <div className="substrate" />
+    </section>
+  );
 }
 
 export default Choise;
