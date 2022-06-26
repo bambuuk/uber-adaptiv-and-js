b4dae1c608fad7c88f444bf5fc669ef9c4495217
Ivan <vanyok.korobka@ukr.net>
2022-06-27 01:04:30 +0300

    moved the Header component to a functional component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,39 +1,29 @@
-import { Component } from 'react';
+import { useState, useContext } from 'react';
 import HeaderView from './HeaderView';
 import ThemeContext from '../../context/ThemeContext';
 import './header.sass';
 
-class Header extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      active: false
-    };
-  }
+function Header() {
+  const [active, setActive] = useState(false);
+  const choiseTheme = useContext(ThemeContext);
 
-  toggleHamburgerMenuActive = (e) => {
+  const toggleHamburgerMenuActive = (e) => {
     if (e.type === 'click' || e.code === 'Enter') {
-      this.setState(({ active }) => ({ active: !active }));
+      setActive(!active);
     }
   };
 
-  render() {
-    const { active } = this.state;
-    const { choiseTheme } = this.context;
-    const styleHamburger = active ? 'hamburger_active' : '';
-    const styleMenu = active ? 'menu_active' : '';
+  const styleHamburger = active ? 'hamburger_active' : '';
+  const styleMenu = active ? 'menu_active' : '';
 
-    return (
-      <HeaderView
-        styleHamburger={styleHamburger}
-        styleMenu={styleMenu}
-        toggleHamburgerMenuActive={this.toggleHamburgerMenuActive}
-        choiseTheme={choiseTheme}
-      />
-    );
-  }
+  return (
+    <HeaderView
+      styleHamburger={styleHamburger}
+      styleMenu={styleMenu}
+      toggleHamburgerMenuActive={toggleHamburgerMenuActive}
+      choiseTheme={choiseTheme}
+    />
+  );
 }
-
-Header.contextType = ThemeContext;
 
 export default Header;
